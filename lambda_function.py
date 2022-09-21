57e02d9deb779124864b071eb6d14969ba09d9af
jcyepez <52051458+jcyepez@users.noreply.github.com>
2022-09-21 11:51:29 -0300

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -12,12 +12,12 @@ class DynamoAccessor:
         dynamo_db = boto3.resource('dynamodb')
         self.table = dynamo_db.Table(dynamo_table)
 
-    def get_data_from_dynamo(self, cc):
-        response = self.table.query(KeyConditionExpression=Key('cc').eq(cc))
+    def get_data_from_dynamo(self, rut):
+        response = self.table.query(KeyConditionExpression=Key('rut').eq(rut))
         return response["Items"][0] if any(response["Items"]) else None
 
 def lambda_handler(event, context):
     dynamo_backend = DynamoAccessor(DYNAMO_BD)
-    db_element = dynamo_backend.get_data_from_dynamo(event['cc'])
+    db_element = dynamo_backend.get_data_from_dynamo(event['rut'])
     return db_element
 
